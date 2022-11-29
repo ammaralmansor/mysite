600031698eb76007b6c3ba9330808ca1a1a8a2b2
ola9999 <89652807+ola9999@users.noreply.github.com>
2022-11-29 17:09:01 +0300

    ola commit

--- mysite/app/models.py
+++ mysite/app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-
 # Create your models here.
+from django.contrib.auth.models import User
+
+from datetime import datetime as dt 
+class New(models.Model):
+   CATEGORY_CHOICES = (
+        ('last', 'last'),
+        ('local', 'local'),
+        ('international', 'international'),
+        ('sport', 'sport'),
+    )
+   LANGUAGE_CHOICES = (
+        ('en', 'en'),
+        ('ar', 'ar'),
+        ('fr', 'fr'),
+    )
+   lang  = models.CharField(choices=LANGUAGE_CHOICES, max_length=13, blank=True, default='en')
+   category = models.CharField(choices=CATEGORY_CHOICES, max_length=13, blank=True, default='last')
+   title = models.TextField(max_length=150,blank=False)
+   creation_datetime = models.DateTimeField(auto_now=True)
+   img_1 = models.ImageField(upload_to='images/%y/&m/%d')
+   img_2 = models.ImageField(upload_to='images/%y/&m/%d')
+   img_3 = models.ImageField(upload_to='images/%y/&m/%d')
+
+   details = models.TextField(max_length=1000,blank=False)
+   
+
+
