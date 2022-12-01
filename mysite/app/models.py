c6820dde24c0bb77c8d04ce7fce0754537c42d15
ammar almansor <ammaralmansor4@gmail.com>
2022-12-01 13:42:14 +0200

    AppRegisteration

--- mysite/app/models.py
+++ mysite/app/models.py
@@ -29,7 +29,31 @@ class Tracking(models.Model):
 
 
 class AppRegisteration(models.Model):
-    pass
+    LANGUAGE_CHOICES = (
+        ('en', 'en'),
+        ('ar', 'ar'),
+        ('fr', 'fr'),
+    )
+    OPERATOR_CHOICES = (
+        ('MTC', 'MTC'),
+        ('Touch', 'Touch'),
+    )
+    PLATEFORM_CHOICES = (
+        ('android', 'android'),
+        ('ios', 'ios'),
+    )
+    MobileNumber = models.IntegerField()
+    operator = models.TextField(
+        max_length=5, blank=False, choices=OPERATOR_CHOICES)
+    AllNotification = models.BooleanField(default=False)
+    ImageNotification = models.BooleanField(default=False)
+    VideoNotification = models.BooleanField(default=False)
+    AudioNotification = models.BooleanField(default=False)
+    platform = models.TextField(
+        max_length=1000, blank=False, choices=PLATEFORM_CHOICES)
+    language_code = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=13, blank=True, default='en')
+    FirebaseToken = models.CharField(max_length=100 , default=None)
 
 
 class New(models.Model):
